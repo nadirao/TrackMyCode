6dcce993813db2f761769a062179e6820b810693
nadiras <nadiraomar98@gmail.com>
2023-05-17 18:29:10 -0500

    wip: added the card component and edited some stuff

--- src/pages/Challenges/Challenges.js
+++ src/pages/Challenges/Challenges.js
@@ -1,6 +1,11 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import MuiDatePicker from "../../components/MUIDatePicker";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import { Typography } from "@material-ui/core";
 
 export default function Challenges(props) {
   const { id } = useParams();
@@ -31,35 +36,42 @@ export default function Challenges(props) {
     <div className="Challenge">
       {loading ? (
         <>
-       <div className="challenge-name">
-          {challenge.name}
-       </div>
-       <div className="challenge-description">
-          {challenge.description.replace(/`/g, "'").split('\n')[0]}
-       </div>
-       <hr/>
-       <div className="challenge-details">
-            <div className="username">Assignee
+          <div className="challenge-name">{challenge.name}</div>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardContent>
+              <div className="challenge-description">
+                {challenge.description.replace(/`/g, "'").split("\n")[0]}
+              </div>
+              <hr />
+              <div className="challenge-details">
+                <div className="username">
+                  Assignee
+                  {/* add username */}
+                </div>
+                <div className="status">
+                  <label>Status</label>
+                  <select>
+                    <option value="todo">To Do</option>
+                    <option value="ip">In Progress</option>
+                    <option value="done">Done</option>
+                    <option value="blocked">Blocked</option>
+                  </select>
+                </div>
+                <div className="due-date">
+                  Due Date
+                  <MuiDatePicker />
+                </div>
+              </div>
+              <hr />
+              <div className="notes">Description
+              <input type="text" placeholder="Write A Note"></input>
+              </div>
 
-            </div>
-            <div className="status">
-            <label>Status</label>
-            <select>
-                <option value="todo">To Do</option>
-                <option value="ip">In Progress</option>
-                <option value="done">Done</option>
-                <option value="blocked">Blocked</option>
-            </select>
-            </div>
-            <div className="due-date">Due Date
-            <MuiDatePicker/>
-            </div>
-       </div>
-       <hr/>
-       <div className="notes">Description
-
-       </div>
-
+            </CardContent>
+            <CardActions>
+              <Button size="small">Save Challenge</Button>
+            </CardActions>
+          </Card>
         </>
       ) : (
         <h1>Loading...</h1>
