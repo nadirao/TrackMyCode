bba575bff4d3157b600c80a8700f6840dd26793d
nadiras <nadiraomar98@gmail.com>
2023-05-17 16:22:08 -0500

    wip: retrived challenge data from api

--- src/pages/Challenges/Challenges.js
+++ src/pages/Challenges/Challenges.js
@@ -1,5 +1,42 @@
-export default function Challenges(){
-    return(
-        <h1>This is the Challenge Page</h1>
-    )
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
+export default function Challenges(props) {
+  const { id } = useParams();
+  const url = `https://www.codewars.com/api/v1/code-challenges/${id}`;
+
+  const [challenge, setChallenge] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const fetchChallenge = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setChallenge(data);
+      setLoading(!loading);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  console.log("challenge:", challenge);
+
+  useEffect(() => {
+    fetchChallenge();
+  }, []);
+
+  return (
+    <div className="Challenge">
+      {loading ? (
+        <>
+          Name: {challenge.name}
+          URL: {challenge.url}
+          Description: {challenge.description.split('\n')[0]}
+        </>
+      ) : (
+        <h1>Loading...</h1>
+      )}
+    </div>
+  );
 }
